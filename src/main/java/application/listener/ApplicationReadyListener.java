5fe6ebba07a1a8904ca4ff8bf0c9b9ccede479ba
MetelevAndrey <Metelev.An.Ol@gmail.com>
2019-04-15 15:04:18 +0300

    context error

--- src/main/java/application/listener/ApplicationReadyListener.java
+++ src/main/java/application/listener/ApplicationReadyListener.java
@@ -3,6 +3,8 @@ package application.listener;
 import application.service.EventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -14,16 +16,19 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class ApplicationReadyListener implements ApplicationListener<ApplicationReadyEvent> {
     private final EventService eventService;
+    @Value("${paths.csv-file}")
+    private String csvFilePath;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
         log.info("Starting csv importing on application ready event...");
 
         try {
-            eventService.importEvents("/home/andrey/IdeaProjects/CSVImporter/src/main/resources/test_case.csv");
+            eventService.importEvents(csvFilePath);
         } catch (IOException e) {
             log.error("Application started wrong, dramatically dies...");
             System.exit(1);
         }
+        eventService.getTopFiveForms();
     }
 }
